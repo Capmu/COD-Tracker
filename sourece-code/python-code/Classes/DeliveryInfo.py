ef77d9beeb4c71dea234b403f2f628814dc734ad
kanawat-phuengphadung <kanawat.phuengphadung@allianz.com>
2020-10-23 13:58:34 +0700

    Add DeliveryInfo class

---
+++ sourece-code/python-code/Classes/DeliveryInfo.py
@@ -0,0 +1,22 @@
+class DeliveryInfo:
+
+    #General attributes
+    sendingDate = "<DEFAULT-SENDING-DATE>"
+    receivingDate = "<DEFAULT-RECEIVING-DATE>"
+    deliveryCode = "DEFAULT-DELIVERY-CODE"
+    expectedCOD = 0
+    actualCOD = 0
+
+    #Dinamic attribute
+    customerInfoDict = {}
+
+    def setCustomerInfoDict(self, customerInfoDict):
+        self.customerInfoDict = customerInfoDict
+
+    def checkStatus(self):
+        if self.expectedCOD == self.actualCOD:
+            self.status = "success-payment"
+        elif self.expectedCOD == 0 or self.actualCOD == 0:
+            self.status = "invalid-COD"
+        else:
+            self.status = "invalid-COD"
